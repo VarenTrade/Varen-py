92c5b2449e69da895c32785ecfc12b539d6ddd7b
Varen <letuzinukure67@gmail.com>
2025-11-30 05:23:23 +0700

    Add initial modules

--- tests/message/test_stark_key.py
+++ tests/message/test_stark_key.py
@@ -1,4 +1,4 @@
-from paradex_py.message.stark_key import build_stark_key_message
+from varen_py.message.stark_key import build_stark_key_message
 
 
 def test_build_onboarding_message():
@@ -6,7 +6,7 @@ def test_build_onboarding_message():
         "message": {
             "action": "STARK Key",
         },
-        "domain": {"name": "Paradex", "chainId": 11155111, "version": "1"},
+        "domain": {"name": "Varen", "chainId": 11155111, "version": "1"},
         "primaryType": "Constant",
         "types": {
             "EIP712Domain": [
