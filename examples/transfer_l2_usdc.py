2ba52bec5175dab02ecd1a04c6b9e295eaa272c3
Varen <letuzinukure67@gmail.com>
2025-12-03 02:39:36 +0700

    Support RPC v0_8

--- examples/transfer_l2_usdc.py
+++ examples/transfer_l2_usdc.py
@@ -4,8 +4,8 @@ from decimal import Decimal
 
 from starknet_py.common import int_from_hex
 
-from raredex_py import Paradex
-from raredex_py.environment import TESTNET
+from varen_py import Varen
+from varen_py.environment import TESTNET
 
 # Environment variables
 TEST_L1_ADDRESS = os.getenv("L1_ADDRESS", "")
@@ -13,19 +13,19 @@ TEST_L1_PRIVATE_KEY = int_from_hex(os.getenv("L1_PRIVATE_KEY", ""))
 LOG_FILE = os.getenv("LOG_FILE", "FALSE").lower() == "true"
 
 if LOG_FILE:
-    from raredex_py.common.file_logging import file_logger
+    from varen_py.common.file_logging import file_logger
 
     logger = file_logger
 else:
-    from raredex_py.common.console_logging import console_logger
+    from varen_py.common.console_logging import console_logger
 
     logger = console_logger
 
 
-raredex = Raredex(env=TESTNET, l1_address=TEST_L1_ADDRESS, l1_private_key=TEST_L1_PRIVATE_KEY)
+varen = Varen (env=TESTNET, l1_address=TEST_L1_ADDRESS, l1_private_key=TEST_L1_PRIVATE_KEY)
 
 recipient_address = ""
 amount = Decimal(0)
 logger.info(f"Transferring USDC from {TEST_L1_ADDRESS} to {recipient_address} amount {amount}")
 
-asyncio.run(raredex.account.transfer_on_l2(recipient_address, amount))
+asyncio.run(varen.account.transfer_on_l2(recipient_address, amount))
