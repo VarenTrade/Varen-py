46604916230bd9e25bb572f9231ea9e081a563c7
Varen <letuzinukure67@gmail.com>
2025-12-10 22:39:44 +0700

    V2: Add Support for Subkeys

--- Varen-py/constants.py
+++ Varen-py/constants.py
@@ -1,7 +1,7 @@
 # The STARK field
 # https://docs.starknet.io/documentation/architecture_and_concepts/Cryptography/p-value/
 PRIME = 2**251 + 17 * 2**192 + 1
-
+ 
 PARACLEAR_DECIMALS = 8
 USDC_DECIMALS = 6
 
